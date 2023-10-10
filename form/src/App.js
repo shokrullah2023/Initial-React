bde7057da2be7154026bd8e124b2c2147394ac4c
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2023-10-10 17:33:37 -0600

    Modifieng Children Using React.Children and React.clonElement & Mouse Event Using Cutting Cross function

--- form/src/App.js
+++ form/src/App.js
@@ -314,4 +314,139 @@ import React from "react";
 //   );
 // }
 
+/*************MODYFING THE CHILDREN USING REACT.CHILDREN AND CLONE***************/
+
+// const Row = ({ children, spacing }) => {
+//   const childStyle = {
+//     marginLeft: `${spacing}px`,
+//   };
+
+//   return (
+//     <div className="Row">
+//       {React.Children.map(children, (child, index) => {
+//         return React.cloneElement(child, {
+//           style: {
+//             ...child.props.style,
+//             ...(index > 0 ? childStyle : {}),
+//           },
+//         });
+//       })}
+//     </div>
+//   );
+// };
+
+// function LiveOrders() {
+//   return (
+//     <div>
+//       <Row spacing={32}>
+//         <p>Pizza Margarita</p>
+//         <p>2</p>
+//         <p>30$</p>
+//         <p>18:30</p>
+//         <p>Shokrullah</p>
+//       </Row>
+//     </div>
+//   );
+// }
+
+/************MOUSE EVENT USING CUTTING-CROSS******************/
+
+// const withMousePosition = (WrappedComponent) => {
+//   return (props) => {
+//     const [mousePosition, setMousePosition] = useState({
+//       x: 0,
+//       y: 0,
+//     });
+
+//     useEffect(() => {
+//       const handleMousePositionChange = (e) => {
+//         setMousePosition({
+//           x: e.clientX,
+//           y: e.clientY,
+//         });
+//       };
+
+//       window.addEventListener("mousemove", handleMousePositionChange);
+//       return () => {
+//         window.removeEventListener("mousemove", handleMousePositionChange);
+//       };
+//     }, []);
+
+//     return <WrappedComponent {...props} mousePosition={mousePosition} />;
+//   };
+// };
+
+// const PanelMouseLogger = ({ mousePosition }) => {
+//   if (!mousePosition) {
+//     return null;
+//   }
+//   return (
+//     <div className="BasicTracker">
+//       <p>Mouse Position</p>
+//       <div className="Row">
+//         <span>x: {mousePosition.x}</span>
+//         <span>y: {mousePosition.y}</span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const PointMouseLogger = ({ mousePosition }) => {
+//   if (!mousePosition) {
+//     return null;
+//   }
+//   return (
+//     <p>
+//       ({mousePosition.x}, {mousePosition.y})
+//     </p>
+//   );
+// };
+
+// const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+// const PointMouseTracker = withMousePosition(PointMouseLogger);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="Header">Little Lemon Restaurant 🍕</header>
+//       <PanelMouseTracker />
+//       <PointMouseTracker />
+//     </div>
+//   );
+// }
+
+/********************************/
+
+const DesertCount = () => {
+  return (
+    <DateFetcher
+      url="https://littlelemon/desserts"
+      render={(data) => {
+        <p>{data.length} desserts</p>;
+      }}
+    />
+  );
+};
+
+const DrinksCount = () => {
+  return (
+    <DataFetcher
+      url="https://littlelemon/drinks"
+      render={(data) => {
+        <h3>{data.length} drinks</h3>;
+      }}
+    />
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <header>Little Lemon Restaurant 🍕</header>
+      <DesertCount />
+      <DrinksCount />
+    </div>
+  );
+}
+
 export default App;
